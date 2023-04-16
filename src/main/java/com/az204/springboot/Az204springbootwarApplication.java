c4c05e25ec1c531aa2e94ac2cb278ff50c6f0d02
Ashish <ashishsonu2000@gmail.com>
2023-04-16 13:01:36 +0530

    commit

---
+++ src/main/java/com/az204/springboot/Az204springbootwarApplication.java
@@ -0,0 +1,13 @@
+package com.az204.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Az204springbootwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Az204springbootwarApplication.class, args);
+	}
+
+}
